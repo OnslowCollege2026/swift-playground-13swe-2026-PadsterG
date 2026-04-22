11ea0822b91f3749d7bc60d51a7464f5732c42fc
Paddy <142456310+PadsterG@users.noreply.github.com>
2026-04-22 00:47:10 +0000

    Created Book and Borrower structs

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+struct Book {
+    let id: Int
+    let title: String
+    let author: String
+    let pages: Int
+    let available: Bool
+}
+
+struct Borrower {
+    let id: Int
+    let firstName: String
+    let lastName: String
+}
 @main
 struct SwiftPlayground {
     static func main() {
